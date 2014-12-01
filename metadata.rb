f1c87d9e403fb4932587ff2c89523ba8698a61c5
Blake Irvin <blake.irvin@yetu.com>
2014-12-01 15:24:07 +0100

    CentOS template support

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'blake.irvin@yetu.com'
 license          'MIT'
 description      'Installs/Configures nad-o-matic'
 long_description 'Installs/Configures nad-o-matic'
-version          '0.2.0'
+version          '0.2.1'
 
 depends 'nad'
