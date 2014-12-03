c2e00a32fb31ccdd5bcc88e9693127921a31138a
Blake Irvin <blake.irvin@yetu.com>
2014-12-03 18:00:22 +0100

    be more data-driven

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,15 +1,15 @@
-template '/opt/circonus/sbin/register_via_nad.sh' do
+template "#{node['nad']['install_path']}/sbin/register_via_nad.sh" do
   source 'register_via_nad.sh.erb'
   mode '0700'
 end
 
-template '/etc/nad.conf' do
+template node['nad']['registration']['configfile'] do
   source 'nad.conf.erb'
   notifies :run, "execute[register_via_nad]"
 end
 
 execute 'register_via_nad' do
-  command '/opt/circonus/sbin/register_via_nad.sh'
+  command "#{node['nad']['install_path']}/sbin/register_via_nad.sh"
   action :nothing
   ignore_failure true
 end
