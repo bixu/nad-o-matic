7d380a319e809ec6ef0831455cdf388e7bb2dfa2
Blake Irvin <blake.irvin@yetu.com>
2014-12-04 15:45:07 +0100

    allow hosts with only public interfaces to register

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,13 @@ default['nad']['install_path']                 = '/opt/circonus'
 default['nad']['bin_path']                     = node['nad']['install_path'] + '/embedded/bin'
 default['nad']['node_path']                    = node['nad']['install_path'] + '/lib/node_modules'
 default['nad']['registration']['authtoken']    = '8c7a057e-18f8-4fe8-8549-63cc965f6dc0'
-default['nad']['registration']['target']       = node.privateaddress
+
+if node['privateaddress']
+  default['nad']['registration']['target']     = node['privateaddress']
+else
+  default['nad']['registration']['target']     = node['ipaddress']
+end
+
 default['nad']['registration']['hostname']     = `hostname`.strip
 default['nad']['registration']['brokerid']     = 335
 default['nad']['registration']['configfile']   = '/etc/nad.conf'
