d3d3ad5c662469813aa68dd4f944972a6fc079a2
farshadfahimi <farshad.fahimi@yetu.com>
2014-12-09 16:26:10 +0100

    switch to cpuuser

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'blake.irvin@yetu.com'
 license          'MIT'
 description      'Installs/Configures nad-o-matic'
 long_description 'Installs/Configures nad-o-matic'
-version          '0.3.1'
+version          '0.4.0'
 
 depends 'nad'
